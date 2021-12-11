24862c40187ef3d7ceac8db011441b5dd4acba0c
Crong (SungEun Kim) <mirxx@naver.com>
2021-12-11 16:58:30 +0900

    웹브라우저 Message 출력 완료

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-const express = require('express')
-const app = express()
-// const port = 3000
-const http = require('http')
-const server = http.createServer(app)
-const { Server } = require("socket.io");
-const io = new Server(server);
-// app.use(express.static(__dirname))
-// app.use(express.static("public"))
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-// app.get('/style.css', function (req, res) {
-//     res.sendFile(__dirname + "/" + '/style.css');
-// })
-
 io.on('connection', (socket) => {
-    console.log(`a user connected`);
-})
+    socket.on('chat message', (msg) => {
+        // io.emit('chat message' + msg);
+        io.emit('chat message', msg);
+    });
+});
 
-server.listen(5000, () => {
-    console.log('Listening on *:5000')
-})
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});

48221eeba1176edf45ce63a6ac3eb5037df5e7d6
Crong (SungEun Kim) <mirxx@naver.com>
2021-12-11 16:04:54 +0900

    index.html stylesheet settings

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express()
 // const port = 3000
 const http = require('http')
 const server = http.createServer(app)
+app.use(express.static(__dirname))
+app.use(express.static("public"))
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/style.css', function (req, res) {
+    res.sendFile(__dirname + "/" + '/style.css');
 })
 
 server.listen(5000, () => {
